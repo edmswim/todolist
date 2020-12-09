e3ff6d8f7001bd8375d8331e1ed5871b32841d2e
Shruti Merchant <shruti.merchant@gmail.com>
2020-12-09 10:21:17 -0800

    Can add new items to the todo list tracking

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
  import addListToArray from './addNewListItem.js';
  import navigationFunctions from './navigationDOM.js';
  import storageItems from './storage.js';
+ import newListItem from './newListItemDOM'
 
 //testing below
 
     //adding values to array
         addListToArray(storageItems.allListItems, "walk ted", "","2020-12-08","highPriority","Dog Stuff");
-        addListToArray(storageItems.allListItems, "walk dog", "","2020-12-07","lowPriority","Dog Stuff");
+        addListToArray(storageItems.allListItems, "walk dog", "","2020-12-07","mediumPriority","Dog Stuff");
         addListToArray(storageItems.allListItems, "walk scoop", "","2020-12-07","lowPriority","Dog Stuff");
         addListToArray(storageItems.allListItems, "get bread", "","2020-12-06","highPriority","Home List");
         addListToArray(storageItems.allListItems, "get cheese", "","2020-12-06","highPriority","Home List");
@@ -15,6 +16,9 @@
 
     //testing filter
     navigationFunctions.updateDom();
+    console.log("test");
+    newListItem.modalOpenClose();
+    newListItem.modalSubmitButton();
 
 
     
@@ -37,8 +41,8 @@ todo list
 
     javascript:
         - new item modal
-        - view description modal
         - edit modal
+        - delete modal
         - upon clicking complete it should refresh...no idea how to do this with our existing structure
-        - fix priorities
+        - fix priorities - medium priority should be yellow
 */
---
+++ src/newListItemDOM.js
@@ -0,0 +1,37 @@
+import addListToArray from './addNewListItem.js';
+import storageItems from './storage.js';
+
+const newListItem = {
+    modalOpenClose: function() {
+        const btn = document.getElementById("addNewItemButton")
+        const newItemModal = document.getElementById('addNewItemModal');
+        const closeModal =document.getElementById('closeAddNewModal');
+        btn.addEventListener("click", function() {addNewItemModal.style.display="block"});
+        closeModal.addEventListener("click", function() {addNewItemModal.style.display="none"})
+        window.onclick = function(event) {if (event.target == newItemModal) {newItemModal.style.display = "none";}}
+    },
+
+    addNewListItem: function() {
+        const title = document.getElementById("newItemForm").elements.namedItem("taskName").value;
+        const description = document.getElementById("newItemForm").elements.namedItem("taskDescription").value;
+        const dueDate = document.getElementById("newItemForm").elements.namedItem("dueDate").value;
+        const project = document.getElementById("newItemForm").elements.namedItem("project").value;
+        const priority = document.getElementById("newItemForm").elements.namedItem("importance").value;
+        addListToArray(storageItems.allListItems, title, description, dueDate, priority, project);
+        console.log(storageItems.allListItems);
+        //you need to have the list refresh here, but first sort out the bug you have
+    },
+
+    modalSubmitButton: function() {
+        const button = document.getElementById("addNewListItemButton");
+        let addItem = newListItem.addNewListItem.bind(newListItem);
+        var formValid = document.getElementById("newItemForm").checkValidity();
+        button.addEventListener("click", function() {
+            if(formValid) {addItem();}
+            else (console.log("Form not complete"))
+        })
+    }
+
+}
+
+export default newListItem
